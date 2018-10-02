84125fa9363ddd5dd177121e26d9adceb5ae4e65
Sébastien OUALLET <sebastien.ouallet@gmail.com>
2018-10-02 22:49:45 +0200

    Implemented add && del friend functionnality

--- src/AppBundle/Controller/FriendController.php
+++ src/AppBundle/Controller/FriendController.php
@@ -5,47 +5,63 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Method;
 use AppBundle\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FriendController extends Controller
 {
+
+    public function __construct(){}
+
     /**
      * Create a new friend relation.
      *
-     * @Route("/friends/add", name="add_friend")
+     * @Route("/friends/add", name="friend_add", methods={"POST"})
      */
     public function addAction(Request $request)
     {
         $response = '';
         $currentUser = $this->getUser();
         $currentUserFriends = $currentUser->getMyFriends();
-        $idToAdd = 3;
 
-        // Get the entity manager
-        $entityManager = $this->getDoctrine()->getManager();
-        // Get the user to delete by his id
-        $userToAdd = $this->getDoctrine()->getRepository(User::class)->findOneBy(array('id' => $idToAdd));
+        $addForm = $this->createFormBuilder()
+                        ->setAction($this->generateUrl('friend_add'))
+                        ->setMethod('POST')
+                        ->add('username', TextType::Class, array('attr' => array('placeholder' => 'Nom d\'utilisateur') ))
+                        ->getForm();
 
-        if (in_array($userToAdd, $currentUserFriends)) {
-            $response = 'Vous êtes déjà ami';
-        } else {
-            // Add the user to currentUser's FriendList
-            $currentUser->addMyFriend($userToAdd);
-            // Save Modification
-            $entityManager->persist($currentUser);
-            $entityManager->flush();
-            // Reponse back
-            $response = "L'utilisateur " . $userToAdd->getUsername() . " a bien été ajouté à votre liste d'ami.";
+        $addForm->handleRequest($request);
+
+        if($addForm->isSubmitted() && $addForm->isValid()){
+            $searchedUsername = $addForm['username']->getData();
+            // Get the entity manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Get the user to delete by his id
+            $userToAdd = $this->getDoctrine()->getRepository(User::class)->findOneBy(array('username' => $searchedUsername));
+
+            if (in_array($userToAdd, $currentUserFriends)) {
+                $response = 'Vous êtes déjà ami.';
+            } else {
+                // Add the user to currentUser's FriendList
+                $currentUser->addMyFriend($userToAdd);
+                // Save Modification
+                $entityManager->persist($currentUser);
+                $entityManager->flush();
+                // Reponse back
+                $response = "L'utilisateur " . $userToAdd->getUsername() . " a bien été ajouté à votre liste d'ami.";
+            }
         }
 
         // Retourne à la page 'profile'
         return $this->redirectToRoute('fos_user_profile_show', array('response' => $response));
+
     }
 
     /**
      * Delete a friend relation.
      *
-     * @Route("/friends/del/{idToDel}", name="del_friend")
+     * @Route("/friends/del/{idToDel}", name="friend_delete")
      */
     public function delAction($idToDel)
     {
--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -7,11 +7,12 @@ use FOS\UserBundle\Controller\ProfileController as BaseController;
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
-
+use AppBundle\Entity\User;
 use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use AppBundle\Controller\FriendController;
 
 
 class ProfileController extends BaseController
@@ -25,19 +26,37 @@ class ProfileController extends BaseController
     {
         $user = $this->container->get('security.token_storage')->getToken()->getUser();
         $friends = $user->getMyFriends();
-
-        // var_dump(is_object($user));
-        // var_dump($user instanceof User);
+        $deleteForms = array();
 
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
+        $addForm = $this->createFormBuilder()
+                        ->setAction($this->generateUrl('friend_add'))
+                        ->setMethod('POST')
+                        ->add('username', TextType::Class, array('attr' => array('placeholder' => 'Nom d\'utilisateur') ))
+                        ->getForm();
+
+        foreach ($friends as $entity) {
+            $deleteForms[$entity->getId()] = $this->createDeleteForm($entity)->createView();
+        }
+   
         return $this->render('@FOSUser/Profile/show.html.twig', array(
             'user' => $user,
-            'friends' => $friends
+            'friends' => $friends,
+            'deleteForms' => $deleteForms,
+            'addForm' => $addForm->createView(),
         ));
     }
 
+    private function createDeleteForm(User $friend)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('friend_delete', array('idToDel' => $friend->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
     
 }
